27a9d1745fdcb1ee44f5eaf34c059c543cb95a9e
OKh <hudoshin7605@gmail.com>
2025-09-05 13:29:18 +0200

    Add test data to db.sql

--- db.sql
+++ db.sql
@@ -83,3 +83,37 @@ alter table sitzplatz
 
 alter table werbung
     add foreign key (menu_id) references menu(menu_id);
+
+insert into film (titel, dauer, inhalt, fsk_freigabe, erscheinungsjahr ) 
+values (
+    'Inception', 148, 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', 13, '2010-07-16'
+),
+('Revolver', 110, 'A gambler named Jake Green is released from prison after serving seven years for a crime he did not commit. He is determined to find the real culprits.', 16, '2005-12-02'),
+('The Dark Knight', 152, 'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.', 16, '2008-07-18'),
+('Interstellar', 169, 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity''s survival.', 13, '2014-11-07'),
+('The Matrix', 136, 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.', 16, '1999-03-31'),
+('Gladiator', 155, 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery', 16, '2000-05-05'),
+('Interstellar', 169, 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity''s survival.', 13, '2014-11-07'),
+('The Matrix', 136, 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.', 16, '1999-03-31'),
+('Gladiator', 155, 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into exile.', 16, '2000-05-05');
+insert into genre (genre) values 
+('Action'),
+('Adventure'),
+('Animation'),
+('Sci-Fi'),
+('Drama'),
+('Thriller'),
+('Crime'),
+('Fantasy'),
+('Comedy'),
+('Biography'),
+('History'),
+('War'),
+('Mystery'),
+('Romance'),
+('Family'),
+('Musical'),
+('Horror'),
+('Sport'),
+('Western'),
+('Historical');
